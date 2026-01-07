b4c36f8427a9d0180bd80ae8f81dc46f009916eb
Alan Cabrera <alancabrera@MacBook-Air-de-Alan.local>
2026-01-07 17:10:04 -0600

    Enable SSL for Postgres connection (AWS RDS fix)

--- app/src/index.js
+++ app/src/index.js
@@ -12,6 +12,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Middleware
